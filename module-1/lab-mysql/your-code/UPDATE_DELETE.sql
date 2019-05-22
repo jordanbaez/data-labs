e17a9a1ba163f511149c72ae20ee6735cf8fb5b3
Jordan Baez <jordan.baez@orange.fr>
2019-05-22 18:26:03 +0200

    labs for numpy

---
+++ module-1/lab-mysql/your-code/UPDATE_DELETE.sql
@@ -0,0 +1,10 @@
+use challenge_2;
+
+UPDATE SALESPERSONS SET STORE='MIAMI' where id = '4'; 
+
+UPDATE BUYERS SET EMAIL='AA@GMAIL.COM' where LAST_NAME = 'AA'; 
+UPDATE BUYERS SET EMAIL='FF@GMAIL.COM' where LAST_NAME = 'FF'; 
+
+Delete from CARS where car_id = '07'  limit 1; 
+    
+#SELECT * FROM BUYERS CARS ;
